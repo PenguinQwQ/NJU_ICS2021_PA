fda2b1ecca92b2d870af77529c7a95bc47936464
tracer-ics2022 <tracer@njuics.org>
2022-09-16 21:14:09 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:14:09 up 10:54,  1 user,  load average: 0.29, 0.14, 0.10

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -25,10 +25,10 @@ $(BINARY): compile_git
 # Some convenient rules
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
-override ARGS += $(ARGS_DIFF)
+override ARGS += --diff=$(DIFF_REF_SO)
 
 # Command to execute NEMU
-IMG := 
+IMG ?= 
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
