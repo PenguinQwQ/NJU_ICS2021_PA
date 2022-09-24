7c46986472486e8b2fde1d25147cfc1c37838ccf
tracer-ics2022 <tracer@njuics.org>
2022-09-24 11:46:30 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:46:30 up  2:17,  1 user,  load average: 0.64, 0.27, 0.20

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -53,10 +53,10 @@ How to ensure the token's precedence?
   {"\\/", TK_DIV},         // divide
   {"\\(", TK_LP},         // left parenthesis
   {"\\)", TK_RP},         // right parenthesis
-  {"[0][x][a-f|A-F|0-9]\\+", TK_HEX}, //Hex Number
-  {"[$][a-z|A-Z|0-9]\\+", TK_REG}, //Reg Name (with $!)
-  {"[0-9][0-9]*", TK_NUMBER }  // Number
-
+  {"[0xa-f|A-F|0-9]+", TK_HEX}, //Hex Number x...
+  {"[$][a-z|A-Z|0-9]+", TK_REG}, //Reg Name (with $!)
+  {"[1-9][0-9]*", TK_NUMBER },  // Number
+  {"[0](?!x)", TK_NUMBER} //Zero Case!
 };
 
 #define NR_REGEX ARRLEN(rules)
