77f717a1080c75b4323aa10e962da2a58712becb
tracer-ics2022 <tracer@njuics.org>
2022-09-24 16:25:32 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:25:32 up  1:27,  1 user,  load average: 0.34, 0.17, 0.16

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -385,7 +385,7 @@ word_t eval(int l, int r)
     word_t rev = -1;
     word_t cur = eval(l + 1, r);
  //   printf("main_op_pos = %u , r = %u  ,cur = %d\n", main_op_pos, r, cur );
-    switch (tokens[main_op_pos].type)
+    switch (tokens[l].type)
     {
     case TK_NEG://TK_NEG
       ans = rev * cur;
