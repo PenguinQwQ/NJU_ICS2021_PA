02e1461142bee6c514218b9a2070dd015bb8a0c1
tracer-ics2022 <tracer@njuics.org>
2022-10-17 14:57:12 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:57:12 up 46 min,  1 user,  load average: 0.49, 0.29, 0.21

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -15,9 +15,9 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  int len_dst = strlen(dst), len_src = strlen(src);
-  if(len_dst < len_src)
-      panic("The Destination String is Too short!! String Overflow!");
+  int len_src = strlen(src);
+//  if(len_dst < len_src)
+//      panic("The Destination String is Too short!! String Overflow!");
   for (int i = 0 ; i <= len_src ; i++) //Including the '\0', so it ends with len_src
       	*(dst + i) = *(src + i);
   return dst;
