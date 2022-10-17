b84b5b99626dfb83c720804dd0ecfd20beb4b3f8
tracer-ics2022 <tracer@njuics.org>
2022-10-17 15:13:52 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:13:52 up  1:03,  1 user,  load average: 0.33, 0.26, 0.20

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,7 +9,7 @@ size_t strlen(const char *s) {
     //If s is a nullptr, then we should return 0;
     if(s == NULL)
       return 0;
-    while((*s != '\0') && (s != NULL))
+    while((s != NULL) && (*s != '\0'))
     {
      len++;	
      s++;   
