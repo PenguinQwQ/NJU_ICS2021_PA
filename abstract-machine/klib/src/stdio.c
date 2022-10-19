f36418455adcb717fc052de36183e7c17b2f32ff
tracer-ics2022 <tracer@njuics.org>
2022-10-19 11:38:38 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:38:38 up  1:18,  1 user,  load average: 0.35, 0.20, 0.11

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,9 +14,57 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+    int d_val = 0;//%d buf
+    char* str = NULL;
+    char s[20000];//%d s buffer
+    int tot = 0;
+    int cnt = 0;
+    int ret = 0;
+    va_list ap;//声明指向参数的指针
+    va_start(ap, fmt);//指针初始化
+    while(fmt != NULL && *fmt != '\0')
+    {
+        //如果读到了%d
+        if((*fmt == '%') && (*(fmt + 1) == 'd'))
+        {
+            ret++;
+            tot = 0;
+            d_val = va_arg(ap, int);
+            while(d_val)
+            {
+                s[++tot] = d_val % 10;
+                d_val /= 10;
+            }
+            while(tot--)
+            {
+                *(out + cnt) = s[tot];
+                cnt++;
+            }
+            fmt++;
+            continue;
+        }
+        //如果读到了%s
+         if((*fmt == '%') && (*(fmt + 1) == 's'))
+        {
+             ret++;
+            str = va_arg(ap, char*);
+            tot = strlen(str);
+			for (int i = 0 ; i < tot ; i++)
+            {
+                *(out + cnt) = *(str + i);
+                cnt++;
+            }
+             fmt++;
+             continue;
+        }
+        //如果读到了其他字符
+        *(out + cnt) = *fmt;
+        cnt++;
+       	fmt++;
+    }
+    va_end(ap);
+  	return ret;
 }
-
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
 }
