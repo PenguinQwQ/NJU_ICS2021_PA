476d2d448c73729d3b34bc593e90fe15e838cf2a
tracer-ics2022 <tracer@njuics.org>
2022-10-19 19:47:46 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:47:46 up  7:16,  1 user,  load average: 0.47, 0.33, 0.30

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -53,15 +53,90 @@ int printf(const char *fmt, ...) {
   int len = 0;
   va_list ap;
   va_start(ap, fmt);
-  len = sprintf(output_buf, fmt, ap);
+  len = vsprintf(output_buf, fmt, ap);
   va_end(ap);
   for (int i = 0 ; i < len ; i++)
     putch(output_buf[i]);
+
   return len;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+   char *s = NULL;//%s str pointer
+   int d_val = 0;//%d integer
+   int len = 0;//string length
+    int num_buf[200];
+    int top = 0;
+   	int cnt = 0;
+    bool sign = false;
+    for (; fmt != NULL && *fmt != '\0' ; fmt++)
+    {
+        if(*fmt != '%')
+        {
+            *out = *fmt;
+            out++;
+            cnt++;
+            continue;
+        }
+        fmt++;//此时,*fmt是%，我们需要跳过！
+        switch(*fmt)
+        {
+            case 's':
+                s = va_arg(ap, char *);
+                len = strlen(s);
+                for (int i = 0 ; i < len ; i++)
+                {
+                    *out = *s;
+                    out++;
+                    s++;
+                    cnt++;
+                }
+                break;
+            case 'd':
+                d_val = 0;
+                d_val += va_arg(ap, int);
+                sign = false;
+                top = 0;
+                if(d_val < 0)
+                {
+                    sign = true;
+                    d_val = -d_val;
+                }
+                while(d_val)
+                {
+                    num_buf[++top] = d_val % 10;
+                    d_val = d_val / 10;
+                }
+                if(top == 0)//也就是,d_val = 0
+                {
+                    *out = '0';
+                    out++;
+                    cnt++;
+                } 
+                else
+                {
+                    if(sign)
+                    {
+                      *out = '-';
+                      out++;
+                      cnt++;
+                    }
+                    while(top)
+                    {
+                        *out = num_buf[top] + '0';
+                        top--;
+                        out++;
+                        cnt++;
+                    }
+                }
+                break;
+              default:
+                   break;
+        }
+        
+    }
+  *out = '\0';//Here is important!
+   return cnt;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
