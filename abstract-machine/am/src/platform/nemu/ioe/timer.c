ae61376e0fe14aaae0582727233acd09c30c1ec0
tracer-ics2022 <tracer@njuics.org>
2022-10-22 00:02:08 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:02:08 up 39 min,  1 user,  load average: 0.14, 0.31, 0.43

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -4,12 +4,12 @@
 
 static uint64_t boot_time = 0;
 void __am_timer_init() {
-  boot_time = ((uint64_t)inl(RTC_ADDR + 4)) + (((uint64_t)inl(RTC_ADDR)) << 32);
+  boot_time = ((uint64_t)inl(RTC_ADDR + 4));// + (((uint64_t)inl(RTC_ADDR)) << 32);
 //  printf("boot time is:%d\n", &boot_time);
   return;
 }
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint64_t now_time =(uint64_t)inl(RTC_ADDR + 4) + (((uint64_t)inl(RTC_ADDR)) << 32);
+  uint64_t now_time =(uint64_t)inl(RTC_ADDR + 4);//+ (((uint64_t)inl(RTC_ADDR)) << 32);
 //  printf("now time is:%d\n", &now_time);
   uptime->us = now_time - boot_time;
 //  printf("us is %d\n", uptime->us);
