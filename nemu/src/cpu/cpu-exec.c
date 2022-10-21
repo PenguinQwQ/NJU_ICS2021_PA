0f504ce84bef0ac8e8230d9dc017ea87fed5b922
tracer-ics2022 <tracer@njuics.org>
2022-10-21 18:56:25 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:56:25 up  2:57,  1 user,  load average: 0.03, 0.11, 0.14

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <cpu/cpu.h>
 #include <cpu/decode.h>
@@ -26,28 +26,47 @@
  * You can modify this value as you want.
  */
 #define MAX_INST_TO_PRINT 10
+#define CONFIG_IRINGBUF true
 
-CPU_state cpu = {}; //here cleared all the registers
+CPU_state cpu = {};           // here cleared all the registers
 uint64_t g_nr_guest_inst = 0; //
-static uint64_t g_timer = 0; // unit: us
+static uint64_t g_timer = 0;  // unit: us
 static bool g_print_step = false;
 void scan_watchpoints();
 void device_update();
+char iringbuf[20][128];
+int iringbuf_cnt = 0;
 
-static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
+static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
+{
 #ifdef CONFIG_ITRACE_COND
-  if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+  if (ITRACE_COND)
+  {
+    log_write("%s\n", _this->logbuf);
+  }
 #endif
-  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
+  if (g_print_step)
+  {
+    IFDEF(CONFIG_ITRACE, puts(_this->logbuf));
+  }
+  IFDEF(CONFIG_IRINGBUF, strcpy(iringbuf[iringbuf_cnt], _this->logbuf));
+  iringbuf_cnt = (iringbuf_cnt + 1) % 20;
+  if (nemu_state.state == NEMU_ABORT)
+  {
+    for (int i = 0 ; i <= iringbuf_cnt; i++)
+      puts(iringbuf[i]);
+  }
+
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   scan_watchpoints();
 }
 
-//This is the API of exec_once needs
-static void exec_once(Decode *s, vaddr_t pc) {
+// This is the API of exec_once needs
+static void exec_once(Decode *s, vaddr_t pc)
+{
   s->pc = pc;
   s->snpc = pc;
-  isa_exec_once(s);//This step, we fetch the instruction and decode
+  isa_exec_once(s); // This step, we fetch the instruction and decode
   cpu.pc = s->dnpc;
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
@@ -55,56 +74,69 @@ static void exec_once(Decode *s, vaddr_t pc) {
   int ilen = s->snpc - s->pc;
   int i;
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
-  for (i = ilen - 1; i >= 0; i --) {
+  for (i = ilen - 1; i >= 0; i--)
+  {
     p += snprintf(p, 4, " %02x", inst[i]);
   }
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
-  if (space_len < 0) space_len = 0;
+  if (space_len < 0)
+    space_len = 0;
   space_len = space_len * 3 + 1;
   memset(p, ' ', space_len);
   p += space_len;
 
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
-      MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
+              MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
 #endif
 }
 
-static void execute(uint64_t n) {
+static void execute(uint64_t n)
+{
   Decode s;
-  for (;n > 0; n --) {
-    exec_once(&s, cpu.pc);//execute n times, in fact
-//calls the exec_once for n times
-    g_nr_guest_inst ++;//add the inst that executed
+  for (; n > 0; n--)
+  {
+    exec_once(&s, cpu.pc); // execute n times, in fact
+    // calls the exec_once for n times
+    g_nr_guest_inst++; // add the inst that executed
     trace_and_difftest(&s, cpu.pc);
-    if (nemu_state.state != NEMU_RUNNING) break;//Once the NEMU isn't running state, quit!
+    if (nemu_state.state != NEMU_RUNNING)
+      break; // Once the NEMU isn't running state, quit!
     IFDEF(CONFIG_DEVICE, device_update());
   }
 }
 
-static void statistic() {
+static void statistic()
+{
   IFNDEF(CONFIG_TARGET_AM, setlocale(LC_NUMERIC, ""));
 #define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%", "%'") PRIu64
   Log("host time spent = " NUMBERIC_FMT " us", g_timer);
   Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
-  if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
-  else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
+  if (g_timer > 0)
+    Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
+  else
+    Log("Finish running in less than 1 us and can not calculate the simulation frequency");
 }
 
-void assert_fail_msg() {
+void assert_fail_msg()
+{
   isa_reg_display();
   statistic();
 }
 
 /* Simulate how the CPU works. */
-void cpu_exec(uint64_t n) {
+void cpu_exec(uint64_t n)
+{
   g_print_step = (n < MAX_INST_TO_PRINT);
-  switch (nemu_state.state) {
-    case NEMU_END: case NEMU_ABORT:
-      printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
-      return;
-    default: nemu_state.state = NEMU_RUNNING;
+  switch (nemu_state.state)
+  {
+  case NEMU_END:
+  case NEMU_ABORT:
+    printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
+    return;
+  default:
+    nemu_state.state = NEMU_RUNNING;
   }
 
   uint64_t timer_start = get_time();
@@ -114,17 +146,21 @@ void cpu_exec(uint64_t n) {
   uint64_t timer_end = get_time();
   g_timer += timer_end - timer_start;
 
-  switch (nemu_state.state) {
-    case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
-//finished the whole procedure, the break
-    case NEMU_END: case NEMU_ABORT:
-      Log("nemu: %s at pc = " FMT_WORD,
-          (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
-           (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
-            ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
-          nemu_state.halt_pc);
-      // fall through
-    case NEMU_QUIT: statistic();
-    case NEMU_STOP: break;
+  switch (nemu_state.state)
+  {
+  case NEMU_RUNNING:
+    nemu_state.state = NEMU_STOP;
+    break;
+    // finished the whole procedure, the break
+  case NEMU_END:
+  case NEMU_ABORT:
+    Log("nemu: %s at pc = " FMT_WORD,
+        (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) : (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) : ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
+        nemu_state.halt_pc);
+    // fall through
+  case NEMU_QUIT:
+    statistic();
+  case NEMU_STOP:
+    break;
   }
 }
