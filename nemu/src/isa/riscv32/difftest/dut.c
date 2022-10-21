97651d046cebc6ec9009896f236a6fb0ca1c2fc5
tracer-ics2022 <tracer@njuics.org>
2022-10-21 20:13:56 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:13:56 up  4:15,  1 user,  load average: 0.89, 0.41, 0.39

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -20,7 +20,7 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for (int i = 0 ; i <= 31 ; i++)
     if(ref_r->gpr[i] != cpu.gpr[i]) return false;
-  if(ref_r->pc != cpu.pc) return false;
+  if(ref_r->pc != pc) return false;
   return true;
 }
 
