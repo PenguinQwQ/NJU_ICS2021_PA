48914e44c142e33f8eee2a3f7b5e42a7112043e0
tracer-ics2022 <tracer@njuics.org>
2022-10-22 21:13:03 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:13:03 up  1:20,  1 user,  load average: 0.12, 0.31, 0.37

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -28,15 +28,15 @@ int atoi(const char* nptr) {
   }
   return x;
 }
-extern char _heap_start;
-static char* ADDR;
+
 
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() - (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  ADDR = &_heap_start;
+extern char _heap_start;
+static char* ADDR = &_heap_start;
   ADDR = ADDR + size;
   return (ADDR - size);
 #endif
