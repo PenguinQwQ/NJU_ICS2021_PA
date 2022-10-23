6e1a21812af8a3051d55ac9e31b166b2f0015c0e
tracer-ics2022 <tracer@njuics.org>
2022-10-23 11:10:43 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:10:43 up  1:06,  1 user,  load average: 0.66, 0.20, 0.11

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -143,7 +143,7 @@ int vsprintf(char *buf, const char *fmt, va_list args)
         if((flags & LEFT) == false)
         	for (int i = 0 ; i + s_len < field_width ; i++)
  				*str++ = c;
-        strcpy(str, arg_str);
+        str = strcpy(str, arg_str);
         str += s_len;
         if(flags & LEFT)
         	for (int i = 0 ; i + s_len < field_width ; i++)
