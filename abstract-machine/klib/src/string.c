32f9d8cb9fed1009c6267e548cb320b1e2f1eb6e
tracer-ics2022 <tracer@njuics.org>
2022-10-23 12:34:34 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:34:34 up  2:30,  1 user,  load average: 0.42, 0.37, 0.45

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -25,46 +25,22 @@ char *strcpy(char *dst, const char *src) {
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  size_t len_src = strlen(src);
-  if(sizeof(dst) < ((n < sizeof(src)) ? n : sizeof(src))) //Check if the dst array holds the src array
-      panic("The Destination String is too short!! String Overflow!");
-  if(len_src + 1 <= n) //包括上'\0'，还是小于count，就整个复制。否则，只复制count个
-  {
-  for (int i = 0 ; i <= len_src ; i++) //Including the '\0', so it ends with len_src
-      	*(dst + i) = *(src + i);
-  }
-  else
-  {
-    for (int i = 0 ; i <= n - 1; i++) //Including the '\0', so it ends with len_src
-      	*(dst + i) = *(src + i);
-  }
+    panic("Not Implemented!");
+}
+
+
+char *strcat(char *dst, const char *src) {
+  assert(dst != NULL);
+  assert(src != NULL);
+  char* pd = dst;
+  const char* ps = src;
+  while(*pd != '\0') pd++;
+  while(*ps != '\0') *pd++ = *ps++;
+  *pd='\0';
   return dst;
+//  panic("Not implemented");
 }
 
-
-char *strcat(char *dst, const char *src) {
-    assert((dst != NULL) && (src != NULL));
-    char* beg = dst;
-    dst = dst + strlen(dst);
-	while(*src != '\0') *dst++ = *src++;
-    *dst = '\0';
-    return beg;
-}
-
-/*
-char *strcat(char *dst, const char *src) {
-    assert((dst != NULL) && (src != NULL));
-     *pos = dst + sizeof(dst) - 1, 
-    char *beg = dst;
-    while(*dst){dst++;}
-    int len_src = strlen(src);
-//    if(pos - dst + 1 < len_src)
-//    	panic("strcat overflow! src+dst > sizeof dst!");
-    for (int i = 0 ; i <= len_src; i++)
-    	*(dst + i) = *(src + i);
-    return beg;
-}
-*/
 int strcmp(const char *s1, const char *s2) {
     assert((s1 != NULL) && (s2 != NULL));
     while((*s1 != '\0') && (*s2 != '\0') && (*s1 == *s2))
@@ -92,54 +68,34 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 
 void *memset(void *s, int c, size_t n) {
 	assert(s);
-    char* tmp = (char *)s;
-    for (int i = 1 ; i <= n && i <= sizeof(tmp); i++)
-    {
-        *tmp = (char)c;
-        tmp++;
-    }
-    return s;
+  unsigned char* p = (unsigned char* )s;
+  unsigned char ch = (unsigned char) c;
+  for(int i=1;i<=n;i++){ *p = ch; p++;}
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-	assert((dst != NULL) && (src != NULL));
-    char* t1 = (char *)dst;
-    char* t2 = (char *)src;
-    for (int i = 1 ; i <= n && i <= sizeof(t1) && i <= sizeof(t2) ; i++)
-    {
-        *t1 = *t2;
-        t1++;
-        t2++;
-    }
-    return dst;
+    panic("Not Implemented!");
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-	assert((out != NULL) && (in != NULL));
-    char* o = (char *)out;
-    char* i = (char *)in;
-    int k = 0;
-    while(n-- && k <= sizeof(o) && k <= sizeof(i))
-    {
-       *o = *i;
-        o++;
-        i++;
-        k++;
-    }
-    return out;
+  assert(out != NULL);
+  unsigned char* p1 = (unsigned char*)out;
+  const char* p2 = (const char*)in;
+  for(int i=1;i<=n;i++) *p1++ = *p2++;
+  return out;
 }
-
 int memcmp(const void *s1, const void *s2, size_t n) {
  	assert((s1 != NULL) && (s2 != NULL));
-    char* x = (char *)s1;
-    char* y = (char *)s2;
-    int k = 0;
-    while(n-- && k <= sizeof(x) && k <= sizeof(y))
-    {
-        if(*x != *y)
-            break;
-        k++;
-    }
-    return *x - *y;
+  int flag = 0;
+  const char* p1 = (const char*)s1; 
+  const char* p2 = (const char*)s2;
+
+  for(int i = 1;i <= n;i++){
+    if(*p1 < *p2){ flag = -8; break; }
+    if(*p1 > *p2){ flag = 8;break; }
+    p1++;p2++;
+  }
+  return flag;
 }
 #endif
