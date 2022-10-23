b29ef4e959a299677d8bd72d96da9129cf1f1e40
tracer-ics2022 <tracer@njuics.org>
2022-10-23 11:02:19 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:02:19 up 58 min,  1 user,  load average: 0.16, 0.08, 0.06

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,10 +5,9 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
+	assert(s != NULL);
     size_t len = 0;
     //If s is a nullptr, then we should return 0;
-    if(s == NULL)
-      return 0;
     while((s != NULL) && (*s != '\0'))
     {
      len++;	
@@ -18,12 +17,11 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  size_t len_src = strlen(src);
-  if(sizeof(dst) < sizeof(src))
-      panic("The Destination String is Too short!! String Overflow!");
-  for (int i = 0 ; i <= len_src ; i++) //Including the '\0', so it ends with len_src
-      	*(dst + i) = *(src + i);
-  return dst;
+    assert((dst != NULL) && (src != NULL));
+    char* beg = dst;
+    while(*src != '\0') *dst++ = *src++;
+    *dst = *src;
+  return beg;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -45,12 +43,12 @@ char *strncpy(char *dst, const char *src, size_t n) {
 
 
 char *strcat(char *dst, const char *src) {
-  	size_t len_dst = strlen(dst), len_src = strlen(src);
-//    if(strlen(dst) + strlen(src) + 1 > sizeof(dst))
- //       panic("strcat overflow! src+dst > sizeof dst!");
-    for (int i = len_dst ; i <= len_dst + len_src; i++)
-    	*(dst + i) = *(src + i - len_dst);
-    return dst;
+    assert((dst != NULL) && (src != NULL));
+    char* beg = dst;
+    dst = dst + strlen(dst);
+	while(*src != '\0') *dst++ = *src++;
+    *dst = '\0';
+    return beg;
 }
 
 /*
@@ -68,12 +66,20 @@ char *strcat(char *dst, const char *src) {
 }
 */
 int strcmp(const char *s1, const char *s2) {
- 	int i = 0, len_s1 = strlen(s1), len_s2 = strlen(s2);
-    while((i <= len_s1 - 1) && (i <= len_s2 - 1) && (s1[i] == s2[i]))
+    assert((s1 != NULL) && (s2 != NULL));
+    while((*s1 != '\0') && (*s2 != '\0') && (*s1 == *s2))
+        s1++,s2++;
+    int flag = 0;
+    if((*s1) < (*s2))
+       {
+        flag = -8;
+       }
+    if((*s1) > (*s2))
         {
-          i++;
+        flag = 8;
         }
-   	return s1[i] - s2[i];
+
+   	return flag;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
