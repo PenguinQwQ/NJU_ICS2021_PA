f2400e1b4f68c9a546c682133f6ac4253e54b625
tracer-ics2022 <tracer@njuics.org>
2022-10-25 13:33:42 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:33:42 up  2:55,  1 user,  load average: 0.56, 0.53, 0.37

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -15,6 +15,7 @@
 
 #include <isa.h>
 #include <memory/paddr.h>
+#include <elf.h>
 
 void init_rand();
 void init_log(const char *log_file);
@@ -44,7 +45,140 @@ void sdb_set_batch_mode();
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
+
 static int difftest_port = 1234;
+
+
+
+
+
+
+struct fuction_unit{
+  char function_name[256];
+  word_t in_addr;
+  word_t out_addr;
+}func[2000];
+static char *elf_file = NULL;
+static char strtab_info[20000];
+uint32_t func_cnt = 0;
+/*
+type = 0:none
+type = 1: call
+type = 2: ret
+
+*/
+void ftrace_display(uint32_t addr)
+{
+  for (int i = 0 ; i < func_cnt ; i++)
+{
+    if(addr == func[i].in_addr)
+    {
+      Log("<%x> call [%s @%x]\n", addr, func[i].function_name, func[i].in_addr);
+      return;
+    }
+    if(addr == func[i].out_addr)
+    {
+      Log("<%x> ret [%s]\n", addr, func[i].function_name);
+      return;      
+    }
+}
+  return;
+}
+
+
+static bool check_elf_info(FILE * fp)
+{
+    fseek(fp, 0, SEEK_SET);//将fp设置到开头
+ 	  Elf32_Ehdr ehdr;   
+    int ret = fread(&ehdr, sizeof(ehdr), 1, fp);
+    if(ret != 1)
+    {
+        Log("Load Error in Elf Header File!!\n");
+        assert(0);
+        return false;
+    }
+
+    if(!((ehdr.e_ident[0] == 0x7f) && (ehdr.e_ident[1] == 'E') && (ehdr.e_ident[2] == 'L') && (ehdr.e_ident[3] == 'F')))
+    {
+      Log("The Elf Header's magic number is wrong!\n");
+      assert(0);
+      return false;
+    }
+
+    if(!(ehdr.e_ident[4] != ELFCLASS32))
+    {
+      Log("The Elf ISA doesn't support riscv32-nemu!\n");
+      assert(0);
+      return false;
+    }
+    
+    if(!(ehdr.e_ident[5] != ELFDATA2LSB))
+    {
+      Log("The Elf data doesn't support Little Endian storage!");
+      assert(0);
+      return false;
+    }
+  return true;
+}
+static void load_elf()
+{
+    if(elf_file == NULL)
+    {
+        Log("There is no elf file to be read. \n ");
+        return;
+    }
+    FILE *fp = fopen(elf_file, "rb");
+    Assert(fp, "Invalid Elf File! Unable to open the file!\n ");
+    if(check_elf_info(fp) == false) return; 
+    Elf32_Ehdr ehdr;
+    fseek(fp, 0, SEEK_END);
+    assert(fread(&ehdr, sizeof(ehdr), 1, fp) == 1);
+    int size = ftell(fp);
+    Log("Elf size = %d\n",size);
+    Elf32_Half sh_cnt = ehdr.e_shnum, sh_sz = ehdr.e_shentsize;
+    Elf32_Off sh_off = ehdr.e_shoff;
+    //next, we are going to find the strtab and symtab to collect the information of fuctions!
+    fseek(fp, sh_off, SEEK_SET);
+    for (int i = 0 ; i < sh_cnt ; i++)
+    {
+      Elf32_Shdr shdr;
+      //move the section header pointer to the now section!
+      fseek(fp, sh_off + i * sh_sz, SEEK_SET);
+      int ret = fread(&shdr, sizeof(Elf32_Shdr), 1, fp);
+      assert(ret == 1);
+      if(shdr.sh_type == SHT_SYMTAB)
+      {
+        Elf32_Shdr symtab = shdr;
+        Elf32_Half symnum = shdr.sh_size / shdr.sh_entsize;
+        Elf32_Shdr strtab;
+        assert(fread(&strtab, sizeof(Elf32_Shdr), 1, fp) == 1);
+        fseek(fp, strtab.sh_offset, SEEK_SET);
+        //store the strtab strings to memory the function name
+        assert(fread(&strtab_info, sizeof(Elf32_Shdr), 1, fp) == 1);
+        fseek(fp, symtab.sh_offset, SEEK_SET);
+        for (Elf32_Half num = 0 ; num < symnum ; num++)
+        {
+          Elf32_Sym sym;
+          assert(fread(&sym, sizeof(Elf32_Sym), 1, fp) == 1);
+          if(ELF32_ST_TYPE(sym.st_info) == STT_FUNC)
+          {
+              func[func_cnt].in_addr = sym.st_value;
+              func[func_cnt].out_addr = sym.st_value + sym.st_size;
+              strcpy(func[func_cnt].function_name, &(strtab_info[sym.st_name]));
+              func_cnt++;
+          }
+        }
+      break;
+      }
+    }
+    fclose(fp);
+    Log("The Elf file loaded successfully!\n");
+}
+
+
+
+
+
 
 static long load_img() {
   if (img_file == NULL) {
@@ -71,6 +205,7 @@ static long load_img() {
 
 static int parse_args(int argc, char *argv[]) {
   const struct option table[] = {
+    {"elf_file" , required_argument, NULL, 'e'},
     {"batch"    , no_argument      , NULL, 'b'},
     {"log"      , required_argument, NULL, 'l'},
     {"diff"     , required_argument, NULL, 'd'},
@@ -85,6 +220,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
+      case 'e': elf_file = optarg; break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
@@ -92,6 +228,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-e,--elf_file=FILE      load the elf file to the nemu\n");
         printf("\n");
         exit(0);
     }
@@ -119,6 +256,9 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Perform ISA dependent initialization. */
   init_isa();
+
+  /*load the elf file to support the ftrace*/
+  load_elf();
 
   /* Load the image to memory. This will overwrite the built-in image. */
   long img_size = load_img();
