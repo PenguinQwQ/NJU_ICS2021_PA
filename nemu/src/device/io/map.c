02c27293da71aed8d82ed2686c7c3cba52d2953b
tracer-ics2022 <tracer@njuics.org>
2022-11-08 16:27:46 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:27:46 up  1:39,  1 user,  load average: 0.14, 0.39, 0.35

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -60,7 +60,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   #ifdef CONFIG_DTRACE
-      Log("Read the device %s @[%x] \n", map->name, addr);
+      Log("[Dtrace] Read the device %s @[%x] \n", map->name, addr);
   #endif
   word_t ret = host_read(map->space + offset, len);
   return ret;
@@ -71,7 +71,7 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   #ifdef CONFIG_DTRACE
-    Log("Write the device %s @[%x] \n", map->name, addr);
+    Log("[Dtrace] Write the device %s @[%x] \n", map->name, addr);
   #endif
 
   host_write(map->space + offset, len, data);
