fade0e4988f42ebb843a068fe54dfe3bbe60f213
tracer-ics2022 <tracer@njuics.org>
2022-11-08 19:29:38 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:29:38 up  1:30,  1 user,  load average: 0.15, 0.04, 0.02

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -57,11 +57,11 @@ void init_map() {
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
-  paddr_t offset = addr - map->low;
-  invoke_callback(map->callback, offset, len, false); // prepare data to read
   #ifdef CONFIG_DTRACE
       Log("[Dtrace] Read the device %s @[%x] \n", map->name, addr);
   #endif
+  paddr_t offset = addr - map->low;
+  invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
   return ret;
 }
