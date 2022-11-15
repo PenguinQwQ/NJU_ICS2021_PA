74edadc21e32e18956bc9246406f36378ec3ac71
tracer-ics2022 <tracer@njuics.org>
2022-11-15 10:20:52 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:20:52 up 39 min,  1 user,  load average: 0.20, 0.16, 0.19

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,7 +8,7 @@
 # define Elf_Addr Elf64_Addr
 # define Elf_Word Elf64_Word
 # define Elf_Xword Elf64_Xword
-
+# define Elf_Off Elf64_Off
 
 #else
 # define Elf_Ehdr Elf32_Ehdr
@@ -17,6 +17,7 @@
 # define Elf_Addr Elf32_Addr
 # define Elf_Word Elf32_Word
 # define Elf_Xword Elf32_Xword
+# define Elf_Off Elf32_Off
 #endif
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
@@ -28,12 +29,31 @@ static uint8_t file_buf[MAX_BUFFER_SIZE];
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //The Elf file is at least 4 bytes!
-  assert(ramdisk_read(file_buf, 0, 52) == 52);
+  assert(ramdisk_read(file_buf, 0, sizeof(Elf_Ehdr)) == sizeof(Elf_Ehdr));
   Elf_Ehdr *elf = (Elf_Ehdr *)file_buf;
   assert(*(uint32_t *)elf->e_ident == 0x464c457f);
+
+  Elf_Off phoff = elf->e_phoff;
+  Elf_Half phnum = elf->e_phnum;
+  Elf_Half phentsize = elf->e_phentsize;
+
+  for (Elf_Half num = 0 ; num < phnum ; num++)
+  {
+    //Read a program header section into file_buf
+    assert(ramdisk_read(file_buf, phoff + num * phentsize, phentsize) == phentsize);
+    Elf_Phdr *phdr = (Elf_Phdr *)file_buf;
+    if(phdr->p_type == PT_LOAD)
+    {
+      printf("Find PT_LOAD!!!\n");
+    }
+  }
+
+
+/*
   printf("The Elf32 Header is %d bytes \n", sizeof(Elf32_Ehdr));
   printf("The Elf64 Header is %d bytes \n", sizeof(Elf64_Ehdr));
   printf("The Nanos Lite Elf Header is %d bytes \n", sizeof(Elf_Ehdr));
+*/
 
 
   return 0;
