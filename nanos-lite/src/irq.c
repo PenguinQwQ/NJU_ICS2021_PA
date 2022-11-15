08ab6c5070ff637a090997579062ad0bcd042de5
tracer-ics2022 <tracer@njuics.org>
2022-11-15 14:46:08 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:46:08 up 58 min,  1 user,  load average: 0.59, 0.49, 0.49

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,11 +1,11 @@
 #include <common.h>
 
-#define EVENT_YIELD 1
-
+void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: Log("Yield Event Occurred!!!"); break; //EVENT_YIELD = 1
+    case EVENT_SYSCALL: Log("Syscall Occurred!!!"); do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
   return c;
