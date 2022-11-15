a07655a20964f062185ec82f711a2e84d48bfe35
tracer-ics2022 <tracer@njuics.org>
2022-11-15 11:32:50 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:32:50 up  1:51,  1 user,  load average: 0.13, 0.20, 0.23

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -71,8 +71,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
-  assert(entry == 6);
-//  assert(entry == 0x830003fc);
+//  assert(entry == 6);
+  assert(entry == 0x830003fc);
   Log("Jump to entry = %d", entry);
   ((void(*)())entry) ();
 }
