baab0f4b06919eb80037502fff6fbe8e4b5a533a
tracer-ics2022 <tracer@njuics.org>
2022-11-16 20:22:05 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:22:05 up  2:00,  1 user,  load average: 0.15, 0.14, 0.16

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -12,9 +12,9 @@ typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
-  size_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
@@ -29,11 +29,15 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
+
+
 /* This is the information about all files in disk. */
+
+
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
@@ -101,3 +105,6 @@ int fs_close(int fd)
 void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
+
+
+
