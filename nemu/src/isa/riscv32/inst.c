9901c68151ddf88164937349024c71ad01bd729d
tracer-ics2022 <tracer@njuics.org>
2022-11-16 20:02:38 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:02:38 up  1:40,  1 user,  load average: 0.44, 0.17, 0.18

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -182,7 +182,6 @@ static int decode_exec(Decode *s) {
   INSTPAT("0100000 ????? ????? 101 ????? 01100 11", sra     ,R  , R(dest) = ((sword_t)src1 >> (sword_t)src2) );
   INSTPAT("0000000 ????? ????? 110 ????? 01100 11", or      ,R  , R(dest) = (src1 | src2) );
   INSTPAT("0000000 ????? ????? 111 ????? 01100 11", and     ,R  , R(dest) = (src1 & src2) );    
-  
 
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall   ,N  , s->dnpc = isa_raise_intr(11 ,s->pc));
   INSTPAT("0011000 00010 00000 000 00000 11100 11", mret    ,N  , s->dnpc = cpu.CSR_REG[MEPC]; cpu.CSR_REG[MSTATUS] &= ~(0x1800););
@@ -195,6 +194,9 @@ static int decode_exec(Decode *s) {
   
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
+  
+  
+  
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
