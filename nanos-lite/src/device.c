05ad404fdeaab4ae6408ce35e0727d4f128fdde1
tracer-ics2022 <tracer@njuics.org>
2022-11-19 15:11:44 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:11:44 up 3 min,  1 user,  load average: 0.97, 0.55, 0.23

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,3 +1,4 @@
+/*
 #include <common.h>
 
 #if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
@@ -35,7 +36,51 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   return strlen(buf);
 }
 
-/*
+
+size_t dispinfo_read(void *buf, size_t offset, size_t len) {
+  AM_GPU_CONFIG_T gpu = io_read(AM_GPU_CONFIG);
+  sprintf(buf, "The Canvas width is %d, height is %d\n", gpu.width, gpu.height);
+  assert(strlen(buf) <= len);
+  return strlen(buf);
+}
+
+size_t fb_write(const void *buf, size_t offset, size_t len) {
+  AM_GPU_CONFIG_T gpu = io_read(AM_GPU_CONFIG);
+  int x = (offset >> 2) % gpu.width, y = (offset >> 2) / gpu.width;
+  io_write(AM_GPU_FBDRAW, x, y, (uint32_t *)buf, len >> 2, 1, true);
+  return len;
+}
+
+void init_device() {
+  Log("Initializing devices...");
+  ioe_init();
+}
+
+*/
+
+#include <common.h>
+
+#if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
+# define MULTIPROGRAM_YIELD() yield()
+#else
+# define MULTIPROGRAM_YIELD()
+#endif
+
+#define NAME(key) \
+  [AM_KEY_##key] = #key,
+
+static const char *keyname[256] __attribute__((used)) = {
+  [AM_KEY_NONE] = "NONE",
+  AM_KEYS(NAME)
+};
+
+size_t serial_write(const void *buf, size_t offset, size_t len) {
+  char *str = (char *)buf;
+  for(int i = 0; i < len; i++)
+    putch(str[i]);
+  return len;
+}
+
 size_t events_read(void *buf, size_t offset, size_t len) {
   char *str = buf;
   AM_INPUT_KEYBRD_T ev=io_read(AM_INPUT_KEYBRD);
@@ -51,19 +96,22 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   assert(ret <= len);
   return ret;
 }
-*/
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T gpu = io_read(AM_GPU_CONFIG);
-  sprintf(buf, "The Canvas width is %d, height is %d\n", gpu.width, gpu.height);
-  assert(strlen(buf) <= len);
-  return strlen(buf);
+  sprintf(buf, "WIDTH:%d\nHEIGHT:%d\n", gpu.width, gpu.height);
+  int ret = strlen(buf);
+  assert(ret <= len);
+  return ret;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T gpu = io_read(AM_GPU_CONFIG);
-  int x = (offset >> 2) % gpu.width, y = (offset >> 2) / gpu.width;
-  io_write(AM_GPU_FBDRAW, x, y, (uint32_t *)buf, len >> 2, 1, true);
+  size_t pix = offset >> 2;
+  int wi = gpu.width;
+  int x, y;
+  y = pix / wi; x = pix % wi;
+  io_write(AM_GPU_FBDRAW, x, y, (uint32_t*)buf, len >> 2, 1, true);
   return len;
 }
 
