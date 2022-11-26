e7351857f292860e13d40712d6d8cdeae064e0d7
tracer-ics2022 <tracer@njuics.org>
2022-11-26 14:13:01 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:13:01 up 8 min,  1 user,  load average: 0.21, 0.35, 0.20

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -12,21 +12,21 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  FILE *fd=fopen(filename,"rb");
-  fseek(fd,0,SEEK_END);
-  int size=ftell(fd);
-  unsigned char * buf=malloc(size);
+  FILE *fd = fopen(filename,"rb");
+  fseek(fd, 0, SEEK_END);
+  int sz = ftell(fd);
+  unsigned char * buf = SDL_malloc(sz);
   fseek(fd,0,SEEK_SET);
 //  printf("File %s,IMG_SIZE:%d\n",filename,size);
-  assert(fread(buf,1,size,fd)==size);
+  assert(fread(buf, 1, sz, fd) == sz);
 //  for(int i=0;i<size;++i) putchar(buf[i]);putchar('\n');
-  SDL_Surface * ans=STBIMG_LoadFromMemory(buf,size);
+  SDL_Surface * IMG = STBIMG_LoadFromMemory(buf, sz);
 //  printf("About to Complete!\n");
-  assert(ans!=NULL);
+  assert(IMG);
   fclose(fd);
   free(buf);
 //  printf("Complete!\n");
-  return ans;
+  return IMG;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,3 +1,4 @@
+//event.c
 #include <NDL.h>
 #include <SDL.h>
 
@@ -8,62 +9,64 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
-static uint8_t kty_st[128];
+static char buf[64];
+static int KEY_NUM = sizeof(keyname) / sizeof(char *);
+static uint8_t key_st[256];
+static bool flag = false;
+
 
 int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
 int SDL_PollEvent(SDL_Event *ev) {   
-    char buf[64];
-    memset(buf, 0, sizeof(buf));
-    bool flag = NDL_PollEvent(buf,sizeof(buf));
-    if(!flag) return 0;
     if(ev == NULL) return 1;
-    if(buf[1] == 'u') ev->type = SDL_KEYUP;
-    else if(buf[1] == 'd') ev->type = SDL_KEYDOWN;
-    int len = strlen(buf);
-    buf[len - 1] = '\0';
-    int size = sizeof(keyname);
-    for(int i = 0;i < size;i++)
-    if(!strcmp(buf + 3,keyname[i])){       
-      ev->key.keysym.sym = i;break;
+    memset(buf, 0, sizeof(buf));
+    if(NDL_PollEvent(buf, sizeof(buf)) == false) return 0;
+    switch (buf[1])
+    {
+    case 'u': ev->type = SDL_KEYUP; break;
+    case 'd': ev->type = SDL_KEYDOWN; break;
+    default : printf("Cannot identify the type of event!!!\n"); break;
     }
-    kty_st[ev->key.keysym.sym] = 1 ^ ev->type;
-   // printf("%s,%d\n",buf,kty_st[ev->key.keysym.sym]);
+    for(int i = 0;i < KEY_NUM;i++)
+    if(strcmp(buf + 3, keyname[i]) == 0){       
+      ev->key.keysym.sym = i;
+      key_st[i] = 1 ^ ev->type;
+      break;
+    }
     return 1;
-    //printf("No implemented!\n");assert(0);return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
     if(event == NULL) return 0;
+    memset(buf, 0, sizeof(buf));
     event->type = SDL_USEREVENT;
     event->key.keysym.sym = 0;
-    char buf[64];
-    memset(buf, 0, sizeof(buf));
-    while(!NDL_PollEvent(buf,sizeof(buf)));   
-      if(buf[1] == 'u') event->type = SDL_KEYUP;
-      else if(buf[1]=='d') event->type = SDL_KEYDOWN;
-      int len = strlen(buf);
-      buf[len - 1] = '\0';
-      int size = sizeof(keyname);
-      for(int i = 0;i < size;i++)
-      if(!strcmp(buf + 3,keyname[i])){       
+    while(NDL_PollEvent(buf, sizeof(buf)) == false);   
+    switch (buf[1])
+    {
+    case 'u': event->type = SDL_KEYUP; break;
+    case 'd': event->type = SDL_KEYDOWN; break;
+    default : printf("Cannot identify the type of event!!!\n"); break;
+    }
+      for(int i = 0;i < KEY_NUM;i++)
+      if(strcmp(buf + 3, keyname[i]) == 0){       
         event->key.keysym.sym = i;
-        //printf("%d,%d\n",SDLK_0,i);
+        key_st[i] = event->type;
         break;
       }
-      kty_st[event->key.keysym.sym] = event->type;
     return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
   return 0;
 }
-
-static bool is_called=0;
 uint8_t* SDL_GetKeyState(int *numkeys) {
-    if(!is_called) memset(kty_st,0,sizeof(kty_st));
-    is_called=1;
-    return kty_st;
+    if(!flag) 
+    {
+      memset(key_st,0,sizeof(key_st));
+      flag = true;
+    }
+    return key_st;
 }
