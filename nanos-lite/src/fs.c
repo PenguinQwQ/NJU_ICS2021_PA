7758979a112f63aad8d0b5c1524875a8a0c41b79
tracer-ics2022 <tracer@njuics.org>
2022-11-26 20:01:16 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:01:16 up  1:13,  1 user,  load average: 0.09, 0.21, 0.32

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,125 +1,3 @@
-/*
-#include <fs.h>
-
-typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
-typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
-
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
-size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-size_t get_ramdisk_size();
-
-size_t serial_write(const void *buf, size_t offset, size_t len);
-size_t events_read(void *buf, size_t offset, size_t len);
-size_t dispinfo_read(void *buf, size_t offset, size_t len);
-size_t fb_write(const void *buf, size_t offset, size_t len);
-
-typedef struct {
-  char *name;
-  size_t size;
-  size_t disk_offset;
-  ReadFn read;
-  WriteFn write;
-  size_t open_offset;
-} Finfo;
-
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENTS, FD_DISPINFO, FD_FB};
-
-size_t invalid_read(void *buf, size_t offset, size_t len) {
-  panic("should not reach here");
-  return 0;
-}
-
-size_t invalid_write(const void *buf, size_t offset, size_t len) {
-  panic("should not reach here");
-  return 0;
-}
-
-static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write, 0},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write, 0},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write, 0},
-  [FD_EVENTS] = {"/dev/events", 0, 0, events_read, invalid_write, 0},
-  [FD_DISPINFO] = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write, 0},
-  [FD_FB] = {"/dev/fb", 0, 0, invalid_read, fb_write, 0},
-#include "files.h"
-};
-
-static uint32_t file_num = sizeof(file_table)/sizeof(Finfo);
-
-size_t fs_open(const char *pathname, int flags, int mode)
-{
-  for (int i = 0 ; i < file_num ; i++)
-  {
-    if(strcmp(file_table[i].name, pathname) == 0)
-    {
-      file_table[i].open_offset = 0;
-      return i;
-    }
-  }
-  Log("The path/name %s file is not found!", pathname);
-  assert(0);
-  return -1;
-}
-
-size_t fs_read(int fd, void *buf, size_t len)
-{
-  assert(buf);
-  if(fd < 0 || fd >= file_num) return -1;//Not exists!
-  if(file_table[fd].read)
-    return file_table[fd].read(buf, file_table[fd].open_offset, len);
-
-  size_t remain_size = file_table[fd].size - file_table[fd].open_offset;
-  len = (remain_size < len) ? remain_size : len;
-  assert(file_table[fd].size >= file_table[fd].open_offset);
-  ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-  file_table[fd].open_offset += len;
-  return len;
-}
-
-size_t fs_write(int fd, void *buf, size_t len)
-{
-  assert(buf);
-  if(fd < 0 || fd >= file_num) return -1;  
-  if(file_table[fd].write)
-    return file_table[fd].write(buf, file_table[fd].open_offset, len);
-  size_t remain_size = file_table[fd].size - file_table[fd].open_offset;
-  len = (remain_size < len) ? remain_size : len;
-  assert(file_table[fd].size >= file_table[fd].open_offset);
-  ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-  file_table[fd].open_offset += len;
-  return len;
-}
-
-size_t fs_lseek(int fd, size_t offset, int whence)
-{
-  if(fd < 0 || fd >= file_num) return -1;
-  printf("offset = %p\n file_table[%d].size = %p\n", offset, fd, file_table[fd].size);
-  assert(offset <= file_table[fd].size);
-  switch (whence)
-  {
-  case SEEK_CUR: file_table[fd].open_offset += offset; break;
-  case SEEK_SET: file_table[fd].open_offset = offset; break;
-  case SEEK_END: file_table[fd].open_offset = file_table[fd].size + offset; break;
-  default: return -1;
-  }
-  return file_table[fd].open_offset;
-}
-
-int fs_close(int fd)
-{
-  file_table[fd].open_offset = 0;
-  return 0;
-}
-
-
-void init_fs() {
-  // TODO: initialize the size of /dev/fb
-  AM_GPU_CONFIG_T gpu = io_read(AM_GPU_CONFIG);
-  file_table[FD_FB].size = gpu.width * gpu.height * sizeof(uint32_t);
-}
-
-*/
-
 #include <fs.h>
 
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
@@ -129,9 +7,9 @@ typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
-  size_t open_offset;
   ReadFn read;
   WriteFn write;
+  size_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENTS, FD_DISPINFO, FD_FB};
@@ -153,103 +31,100 @@ size_t fb_write(const void *buf, size_t offset, size_t len);
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
-  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
-  [FD_EVENTS] = {"/dev/events", 0, 0, 0, events_read, invalid_write},
-  [FD_DISPINFO] = {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
-  [FD_FB] = {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
+  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write, 0},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write, 0},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write, 0},
+  [FD_EVENTS] = {"/dev/events", 0, 0, events_read, invalid_write, 0},
+  [FD_DISPINFO] = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write, 0},
+  [FD_FB] = {"/dev/fb", 0, 0, invalid_read, fb_write, 0},
 #include "files.h"
 };
 
+static int file_num = sizeof(file_table) / sizeof(Finfo);
+static bool flag = 0;
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t get_ramdisk_size();
 
 void init_fs() {
   AM_GPU_CONFIG_T gpu = io_read(AM_GPU_CONFIG);
-  file_table[FD_FB].size = gpu.width * gpu.height * 4;
-  // TODO: initialize the size of /dev/fb
+  file_table[FD_FB].size = gpu.width * gpu.height * sizeof(uint32_t);
 }
-
 int fs_open(const char *pathname, int flags, int mode){
-  int nfs = sizeof(file_table)/sizeof(Finfo);
-  bool is_find = 0;
-  int ret = 0;
-  for(int i = 0; i < nfs; i++){
-    if(strcmp(pathname,file_table[i].name) == 0){
+  flag = false;
+  int ret = -1;
+  for(int i = 0; i < file_num; i++){
+    if(strcmp(pathname, file_table[i].name) == 0){
       ret = i;
-      is_find = 1;
+      flag = true;
       break;
     }
   }
-  if(!is_find){
-    panic("%s: No such file !\n",pathname);
+  if(flag == false){
+    printf("The path: %s file not found!!!\n", pathname);
+    assert(0);
   }
   return ret;
 }
 
 size_t fs_read(int fd, void *buf, size_t len){
+  assert(buf != NULL);
+  if(fd < 0 || fd >= file_num)
+    {
+      panic("Invalid File Describer!!!");
+      return -1;
+    }
+  
+  //Check The inner VFS read function and call it directly!
   if(file_table[fd].read)
     return file_table[fd].read(buf, file_table[fd].open_offset, len);
-  
-  assert(buf);
   size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
-
   assert(file_table[fd].size >= file_table[fd].open_offset);
-
-  size_t readable = file_table[fd].size - file_table[fd].open_offset;
-  
-  if(readable == 0) return 0;
-
-  size_t bytes = len;
-  if(readable < len) bytes = readable;
-
-  ramdisk_read(buf, offset, bytes);
-  file_table[fd].open_offset += bytes;
-
-  return bytes;
+  size_t read_len = file_table[fd].size - file_table[fd].open_offset;
+  if(read_len == 0) return 0;
+  size_t read_bytes = len;
+  read_bytes = (read_len < len) ? read_len : read_bytes;
+  ramdisk_read(buf, offset, read_bytes);
+  file_table[fd].open_offset += read_bytes;
+  return read_bytes;
 }
 
 size_t fs_write(int fd, const void *buf, size_t len){
+  assert(buf);
+  if(fd < 0 || fd >= file_num)
+    {
+      panic("Invalid File Describer!!!");
+      return -1;
+    }
+
   if(file_table[fd].write)
     return file_table[fd].write(buf, file_table[fd].open_offset, len);
-  
-  //assert(fd >= 4);
-  assert(buf);
   size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
-
   assert(file_table[fd].size >= file_table[fd].open_offset);
-
-  size_t writable = file_table[fd].size - file_table[fd].open_offset;
-  
-  if(writable == 0) return 0;
-
-
-  size_t bytes = len;
-  if(writable < len) bytes = writable;
-
-  ramdisk_write(buf, offset, bytes);
-  file_table[fd].open_offset += bytes;
-
-  return bytes;
+  size_t write_len = file_table[fd].size - file_table[fd].open_offset;
+  if(write_len == 0) return 0;
+  size_t write_bytes = len;
+  write_bytes = (write_len < len) ? write_len : write_bytes;
+  ramdisk_write(buf, offset, write_bytes);
+  file_table[fd].open_offset += write_bytes;
+  return write_bytes;
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence){
-  printf("fd= %d , offset = %p, file_table[fd].open_offset = %p\n", fd, offset, file_table[fd].open_offset);
+  //Check the file describer is valid!
+    if(fd < 0 || fd >= file_num)
+    {
+      panic("Invalid File Describer!!!");
+      return -1;
+    }
+  //Check the offset&size is compatiable.
+  assert(offset <= file_table[fd].size);
+//  printf("fd= %d , offset = %p, file_table[fd].open_offset = %p\n", fd, offset, file_table[fd].open_offset);
   switch (whence){
-  case SEEK_SET:
-    file_table[fd].open_offset = offset;
-    break;
-  case SEEK_CUR:
-    file_table[fd].open_offset += offset;
-    break;
-  case SEEK_END:
-    file_table[fd].open_offset = file_table[fd].size + offset;
-    break;
-  default: 
-    assert(0);
-    break;
+  case SEEK_SET: file_table[fd].open_offset = offset; break;
+  case SEEK_CUR: file_table[fd].open_offset += offset; break;
+  case SEEK_END: file_table[fd].open_offset = file_table[fd].size + offset; break;
+  default: printf("Invalid Pointer Placement!!!\n"); assert(0); break;
   }
   return file_table[fd].open_offset;
 }
