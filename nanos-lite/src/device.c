a7bcb0bfc93db8145409ee5c524a98fc9ef089be
tracer-ics2022 <tracer@njuics.org>
2022-11-26 15:48:33 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:48:33 up  1:43,  1 user,  load average: 0.52, 0.33, 0.28

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,5 +1,3 @@
-//device.c
-/*
 #include <common.h>
 
 #if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
@@ -56,67 +54,4 @@ void init_device() {
   Log("Initializing devices...");
   ioe_init();
   disp_init();
-}
-
-*/
-#include <common.h>
-
-#if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
-# define MULTIPROGRAM_YIELD() yield()
-#else
-# define MULTIPROGRAM_YIELD()
-#endif
-
-#define NAME(key) \
-  [AM_KEY_##key] = #key,
-
-static const char *keyname[256] __attribute__((used)) = {
-  [AM_KEY_NONE] = "NONE",
-  AM_KEYS(NAME)
-};
-
-size_t serial_write(const void *buf, size_t offset, size_t len) {
-  char *str = (char *)buf;
-  for(int i = 0; i < len; i++)
-    putch(str[i]);
-  return len;
-}
-
-size_t events_read(void *buf, size_t offset, size_t len) {
-  char *str = buf;
-  AM_INPUT_KEYBRD_T ev=io_read(AM_INPUT_KEYBRD);
-  bool isdown = ev.keydown;
-  int code = ev.keycode;
-  if(code == 0) return 0;
-  str[0] = 0;
-  if(isdown) strcat(buf,"kd ");
-  else strcat(buf,"ku ");
-  strcat(buf,keyname[code]);
-  strcat(buf,"\n");
-  int ret = strlen(buf);
-  assert(ret <= len);
-  return ret;
-}
-
-size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  AM_GPU_CONFIG_T gpu = io_read(AM_GPU_CONFIG);
-  sprintf(buf, "WIDTH:%d\nHEIGHT:%d\n", gpu.width, gpu.height);
-  int ret = strlen(buf);
-  assert(ret <= len);
-  return ret;
-}
-
-size_t fb_write(const void *buf, size_t offset, size_t len) {
-  AM_GPU_CONFIG_T gpu = io_read(AM_GPU_CONFIG);
-  size_t pix = offset >> 2;
-  int wi = gpu.width;
-  int x, y;
-  y = pix / wi; x = pix % wi;
-  io_write(AM_GPU_FBDRAW, x, y, (uint32_t*)buf, len >> 2, 1, true);
-  return len;
-}
-
-void init_device() {
-  Log("Initializing devices...");
-  ioe_init();
 }
