72f30a15efffa09a6bd6f4d29ec30010aa636a54
tracer-ics2022 <tracer@njuics.org>
2022-11-26 19:36:15 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:36:15 up 48 min,  1 user,  load average: 0.54, 0.42, 0.23

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -22,6 +22,7 @@ static void sh_prompt() {
   sh_printf("sh> ");
 }
 
+/*
 static void sh_handle_cmd(const char *cmd) {
 }
 
@@ -29,6 +30,52 @@ void builtin_sh_run() {
   sh_banner();
   sh_prompt();
 
+  while (1) {
+    SDL_Event ev;
+    if (SDL_PollEvent(&ev)) {
+      if (ev.type == SDL_KEYUP || ev.type == SDL_KEYDOWN) {
+        const char *res = term->keypress(handle_key(&ev));
+        if (res) {
+          sh_handle_cmd(res);
+          sh_prompt();
+        }
+      }
+    }
+    refresh_terminal();
+  }
+}
+*/
+static void sh_handle_cmd(const char *cmd) {
+  char* cmd_p=(char*)cmd;
+  char* c_name=strtok(cmd_p," \n");
+  if (c_name==NULL) return;
+  else if(!strcmp(c_name,"echo")){
+    char* str=strtok(NULL," ");
+    sh_printf("%s",str);
+  }
+  else if(!strcmp(c_name,"exit")){
+    exit(0);
+  }
+  else{
+    char* c_args=strtok(NULL," \n");
+    char* args[]={c_name,c_args,NULL};
+    char* envp[]={NULL};
+      int ret=execvp(c_name,args);
+    if(ret==-1){
+      if(c_name[0]=='/'){
+          if(execve(c_name,args,envp)==-1) sh_printf("No such file \"%s\"\n",c_name);
+        }
+        else sh_printf("No such command \"%s\"\n",c_name);
+    }
+  }
+}
+
+void builtin_sh_run() {
+  sh_banner();
+  sh_prompt();
+
+  setenv("PATH", "/bin:/usr/bin", 0);
+  
   while (1) {
     SDL_Event ev;
     if (SDL_PollEvent(&ev)) {
