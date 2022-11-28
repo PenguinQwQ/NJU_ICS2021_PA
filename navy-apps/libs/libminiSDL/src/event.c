428e1276b47a7fbfbec53f31aed889a086d962c8
tracer-ics2022 <tracer@njuics.org>
2022-11-28 10:58:24 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:58:24 up 26 min,  1 user,  load average: 0.78, 0.43, 0.26

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -84,29 +84,35 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
-static uint8_t kty_st[128];
+static uint8_t key_state[128];
+static char buf[256];
+static int sz = sizeof(keyname);
+
 
 int SDL_PushEvent(SDL_Event *ev) {
-    if(ev==NULL) return 0;
-    printf("No implemented!\n");assert(0);
+    if(ev == NULL) return 0;
+//    panic("SDL_PushEVENT not implemented!!");
     return 0;
 }
 
 int SDL_PollEvent(SDL_Event *ev) {   
-    char buf[64];memset(buf,0,sizeof(buf));
-    bool flag=NDL_PollEvent(buf,sizeof(buf));
-    if(!flag) return 0;
-    if(ev==NULL) return 1;
-    if(buf[1]=='u') ev->type=SDL_KEYUP;
-    else if(buf[1]=='d') ev->type=SDL_KEYDOWN;
-    int len=strlen(buf);
-    buf[len-1]='\0';
-    int size=sizeof(keyname);
-    for(int i=0;i<size;i++)
-    if(!strcmp(buf+3,keyname[i])){       
-      ev->key.keysym.sym=i;break;
+    memset(buf, 0, sizeof(buf));
+   if(NDL_PollEvent(buf,sizeof(buf)) == false) return 0;
+   if(ev == NULL) return 1;
+
+    switch (buf[1])
+    {
+    case 'u': ev->type = SDL_KEYUP; break;
+    case 'd': ev->type = SDL_KEYDOWN; break;
+    default : printf("Cannot identify the type of event!!!\n"); break;
     }
-    kty_st[ev->key.keysym.sym]=1^ev->type;
+    buf[strlen(buf) - 1] = '\0';
+    for(int i = 0;i < sz;i++)
+    if(strcmp(buf+3,keyname[i]) == false){       
+      ev->key.keysym.sym=i;
+      break;
+    }
+    key_state[ev->key.keysym.sym] = 1^ev->type;
    // printf("%s,%d\n",buf,kty_st[ev->key.keysym.sym]);
     return 1;
     //printf("No implemented!\n");assert(0);return 0;
@@ -128,7 +134,7 @@ int SDL_WaitEvent(SDL_Event *event) {
         //printf("%d,%d\n",SDLK_0,i);
         break;
       }
-      kty_st[event->key.keysym.sym]=event->type;
+      key_state[event->key.keysym.sym]=event->type;
     return 1;
 }
 
@@ -138,8 +144,8 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 
 static bool is_called=0;
 uint8_t* SDL_GetKeyState(int *numkeys) {
-    if(!is_called) memset(kty_st,0,sizeof(kty_st));
+    if(!is_called) memset(key_state,0,sizeof(key_state));
     is_called=1;
-    return kty_st;
+    return key_state;
 }
 
