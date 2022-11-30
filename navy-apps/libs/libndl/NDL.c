d6fda29bf0105b2be531ecfe84f3eccf2ea68338
tracer-ics2022 <tracer@njuics.org>
2022-11-30 19:22:59 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:22:59 up 37 min,  1 user,  load average: 0.83, 0.54, 0.34

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -96,9 +96,9 @@ int NDL_Init(uint32_t flags) {
 //  int fd = open("/proc/dispinfo", 0, 0);
 //  assert(read(fd, file_buf, FILE_BUF_SIZE));
 
-//  FILE *fp = fopen("/proc/dispinfo", "r");
-//  fscanf(fp, "WIDTH:%d\nHEIGHT:%d\n", &screen_w, &screen_h);
-//  printf("screen_w is %d, screen_h is %d \n", screen_w, screen_h);
+  FILE *fp = fopen("/proc/dispinfo", "r");
+  fscanf(fp, "WIDTH:%d\nHEIGHT:%d\n", &screen_w, &screen_h);
+  printf("screen_w is %d, screen_h is %d \n", screen_w, screen_h);
 /*
   strtok(file_buf,":\n");
   //Get screen_w
