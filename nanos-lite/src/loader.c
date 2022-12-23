e0037721b609472506fdd00033dfdc1976c63215
tracer-ics2022 <tracer@njuics.org>
2022-12-23 21:14:09 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:14:09 up 46 min,  1 user,  load average: 0.35, 0.28, 0.20

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -123,26 +123,11 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg){
   printf("kcontext地址为:%p\n", pcb->cp);
 }
 
-static size_t ceil_4_bytes(size_t size){
-  if (size & 0x3)
-    return (size & (~0x3)) + 0x4;
-  return size;
-}
 
 
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]){
-  int envc = 0, argc = 0;
   AddrSpace *as = &pcb->as;
   protect(as);
-  
-  if (envp){
-    for (; envp[envc]; ++envc){}
-  }
-  if (argv){
-    for (; argv[argc]; ++argc){}
-  }
-  char *envp_ustack[envc];
-
   void *alloced_page = new_page(NR_PAGE) + NR_PAGE * 4096; //得到栈顶
 
   //这段代码有古怪，一动就会出问题，莫动
@@ -159,46 +144,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   map(as, as->area.end - 1 * PAGESIZE, alloced_page - 1 * PAGESIZE, 1); 
   
   char *brk = (char *)(alloced_page - 4);
-  // 拷贝字符区
-  for (int i = 0; i < envc; ++i){
-    brk -= (ceil_4_bytes(strlen(envp[i]) + 1)); // 分配大小
-    envp_ustack[i] = brk;
-    strcpy(brk, envp[i]);
-  }
-
-  char *argv_ustack[envc];
-  for (int i = 0; i < argc; ++i){
-    brk -= (ceil_4_bytes(strlen(argv[i]) + 1)); // 分配大小
-    argv_ustack[i] = brk;
-    strcpy(brk, argv[i]);
-  }
-  
   intptr_t *ptr_brk = (intptr_t *)(brk);
-
-  // 分配envp空间
-  ptr_brk -= 1;
-  *ptr_brk = 0;
-  ptr_brk -= envc;
-  for (int i = 0; i < envc; ++i){
-    ptr_brk[i] = (intptr_t)(envp_ustack[i]);
-  }
-
-  // 分配argv空间
-  ptr_brk -= 1;
-  *ptr_brk = 0;
-  ptr_brk = ptr_brk - argc;
-  
-  // printf("%p\n", ptr_brk);
-  printf("%p\t%p\n", alloced_page, ptr_brk);
-  //printf("%x\n", ptr_brk);
-  //assert((intptr_t)ptr_brk == 0xDD5FDC);
-  for (int i = 0; i < argc; ++i){
-    ptr_brk[i] = (intptr_t)(argv_ustack[i]);
-  }
-
-  ptr_brk -= 1;
-  *ptr_brk = argc;
-  
   //这条操作会把参数的内存空间扬了，要放在最后
   uintptr_t entry = loader(pcb, filename);
   Area karea;
@@ -207,17 +153,8 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 
   Context* context = ucontext(as, karea, (void *)entry);
   pcb->cp = context;
-
-  printf("新分配ptr=%p\n", as->ptr);
-  printf("UContext Allocted at %p\n", context);
-  printf("Alloced Page Addr: %p\t PTR_BRK_ADDR: %p\n", alloced_page, ptr_brk);
-
   ptr_brk -= 1;
   *ptr_brk = 0;//为了t0_buffer
   //设置了sp
   context->gpr[2]  = (uintptr_t)ptr_brk - (uintptr_t)alloced_page + (uintptr_t)as->area.end;
-
-  //似乎不需要这个了，但我还不想动
-  context->GPRx = (uintptr_t)ptr_brk - (uintptr_t)alloced_page + (uintptr_t)as->area.end + 4;
-  //context->GPRx = (intptr_t)(ptr_brk);
 }
