c5d8b2074a5ca171d530f0f20635760a689e970b
tracer-ics2022 <tracer@njuics.org>
2022-12-23 21:22:40 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:22:40 up 54 min,  1 user,  load average: 0.18, 0.22, 0.18

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -34,7 +34,6 @@ static void read(int fd, void *buf, size_t offset, size_t len){
   void *page_start = new_page(page_n);
 
   printf("Loaded Segment from [%x to %x)\n", vaddr, vaddr + p_memsz);
-  
   for (int i = 0; i < page_n; ++i){
     // TODO: 这里prot参数不规范
     map(as, (void *)((vaddr & ~0xfff) + i * PAGESIZE), (void *)(page_start + i * PAGESIZE), 1);
@@ -50,60 +49,32 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   if (fd == -1){ 
     assert(0); //filename指向文件不存在
   }
-  
   AddrSpace *as = &pcb->as;
-  
   Elf_Ehdr elf_header;
   read(fd, &elf_header, 0, sizeof(elf_header));
   //根据小端法 0x7F E L F
   assert(*(uint32_t *)elf_header.e_ident == 0x464c457f);
-  
   ELF_Off program_header_offset = elf_header.e_phoff;
   size_t headers_entry_size = elf_header.e_phentsize;
   int headers_entry_num = elf_header.e_phnum;
-
   for (int i = 0; i < headers_entry_num; ++i){
     Elf_Phdr section_entry;
     read(fd, &section_entry, 
       i * headers_entry_size + program_header_offset, sizeof(elf_header));
     void *phys_addr;
     uintptr_t virt_addr;
-    switch (section_entry.p_type) {
-    case PT_LOAD:
-      //virt_addr = (void *)section_entry.p_vaddr; 
-      // phys_addr = (void *)alloced_page_start + (section_entry.p_vaddr - 0x40000000); // 这里是把0x40000000加载到他对应的实际地址
+    if(section_entry.p_type == PT_LOAD) {
       virt_addr = section_entry.p_vaddr;
       phys_addr = alloc_section_space(as, virt_addr, section_entry.p_memsz);
-
-      // printf("Load to %x with offset %x\n", phys_addr, section_entry.p_offset);
-      //做一个偏移
       read(fd, phys_addr + (virt_addr & 0xfff), section_entry.p_offset, section_entry.p_filesz);
       //同样做一个偏移
       memset(phys_addr + (virt_addr & 0xfff) + section_entry.p_filesz, 0, 
         section_entry.p_memsz - section_entry.p_filesz);
-      
       if (section_entry.p_filesz < section_entry.p_memsz){// 应该是.bss节
-        //做一个向上的4kb取整数
-        // if (pcb->max_brk == 0){
-        printf("Setting .bss end %x\n", section_entry.p_vaddr + section_entry.p_memsz);
-        // 我们虽然用max_brk记录了最高达到的位置，但是在新的PCB中，我们并未在页表目录中更新这些信息，oH，所以就会失效啦。
-        // 于是我们就做了一些权衡。
-        //pcb->max_brk = MAX(pcb->max_brk, ROUNDUP(section_entry.p_vaddr + section_entry.p_memsz, 0xfff));
-        //TODO: Trade-off
         pcb->max_brk = ROUNDUP(section_entry.p_vaddr + section_entry.p_memsz, 0xfff);
-        
-        // }
       }
-      
-      break;
-    
-    default:
-      break;
     }
-
   }
-  
-  printf("Entry: %p\n", elf_header.e_entry);
   return elf_header.e_entry;
 }
 
@@ -118,12 +89,8 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg){
   Area karea;
   karea.start = &pcb->cp;
   karea.end = &pcb->cp + STACK_SIZE;
-
   pcb->cp = kcontext(karea, entry, arg);
-  printf("kcontext地址为:%p\n", pcb->cp);
 }
-
-
 
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]){
   AddrSpace *as = &pcb->as;
