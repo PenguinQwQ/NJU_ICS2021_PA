e5c4daa0828312a3f28b0f1481574f560419c0eb
tracer-ics2022 <tracer@njuics.org>
2022-12-28 20:19:35 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:19:35 up 50 min,  1 user,  load average: 0.39, 0.33, 0.29

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,6 +16,22 @@
 # define ELF_Off Elf32_Off
 #endif
 
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_X86__)
+# define EXPECT_TYPE EM_386
+#elif defined(__ISA_MIPS32__)
+# define EXPECT_TYPE EM_MIPS_X
+#elif defined(__ISA_RISCV32__)
+# define EXPECT_TYPE EM_RISCV
+#elif defined(__ISA_RISCV64__)
+# define EXPECT_TYPE EM_RISCV
+#else
+# error Unsupported ISA
+#endif
+
+
+
 int fs_open(const char *pathname, int flags, int mode);
 size_t fs_read(int fd, void *buf, size_t len);
 size_t fs_write(int fd, const void *buf, size_t len);
@@ -24,9 +40,8 @@ int fs_close(int fd);
 
 
 #define NR_PAGE 8
-#define PAGESIZE 4096
 #define PG_MASK 0xfff
-
+#define PG_SIZE 4096
 #define MAX(a, b)((a) > (b) ? (a) : (b))
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
@@ -35,41 +50,44 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     printf("The file %s not found!", filename);
     assert(0); 
   }
-  AddrSpace *as = &pcb->as;
-  Elf_Ehdr elf_header;
+  Elf_Ehdr ehdr;
   assert(fs_lseek(fd, 0, SEEK_SET) != -1);
-  assert(fs_read(fd, &elf_header, sizeof(Elf_Ehdr)) == sizeof(Elf_Ehdr));
+  assert(fs_read(fd, &ehdr, sizeof(Elf_Ehdr)) == sizeof(Elf_Ehdr));
 
-  assert(*(uint32_t *)elf_header.e_ident == 0x464c457f);
+  assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
 
-  ELF_Off phpos = elf_header.e_phoff;
-  size_t phsize = elf_header.e_phentsize;
-  int headers_entry_num = elf_header.e_phnum;
-  for (int i = 0; i < headers_entry_num; ++i){
+  ELF_Off phpos = ehdr.e_phoff;
+  size_t phsize = ehdr.e_phentsize;
+  int cnt = ehdr.e_phnum;
+  for (int i = 0; i < cnt; ++i){
     Elf_Phdr phdr;
     assert(fs_lseek(fd, phpos, SEEK_SET) != -1);
     assert(fs_read(fd, &phdr, sizeof(Elf_Phdr)) == sizeof(Elf_Phdr));
     phpos += phsize;
 
-    if(phdr.p_type == PT_LOAD) {
-    void *p_addr;
+    if(phdr.p_type == PT_LOAD) 
+    {
+      void *p_addr;
       uintptr_t v_addr = phdr.p_vaddr;
-      size_t page_num = ((v_addr + phdr.p_memsz - 1) >> 12) - (v_addr >> 12) + 1;
-      void *page_start = new_page(page_num);
-      for (int i = 0; i < page_num; ++i)
-          map(as, (void *)((v_addr & ~PG_MASK) + i * PAGESIZE), (void *)(page_start + i * PAGESIZE), 1);
-      p_addr = page_start;
+      size_t pg_num = ((v_addr + phdr.p_memsz - 1) >> 12) - (v_addr >> 12) + 1;
+      void *pg_st = new_page(pg_num);
+      uintptr_t pg_off = 0, pg_base = (v_addr & (~PG_MASK)), v_off = (v_addr & PG_MASK);
+      while(pg_num--)
+      { 
+          map(&pcb->as, (void *)(pg_base + pg_off), (void *)(pg_st + pg_off), 1);
+          pg_off += PG_SIZE;
+      }
+      p_addr = pg_st;
       assert(fs_lseek(fd, phdr.p_offset, SEEK_SET) != -1);
       uint32_t filesz = phdr.p_filesz, memsz = phdr.p_memsz;
+      assert(fs_read(fd, p_addr + v_off, filesz) == filesz);
       assert(filesz <= memsz);
-      assert(fs_read(fd, p_addr + (v_addr & PG_MASK), filesz) == filesz);
-      memset(p_addr + (v_addr & PG_MASK) + filesz, 0, memsz - filesz);
-      if (filesz < memsz){// 应该是.bss节
+      memset(p_addr + v_off + filesz, 0, memsz - filesz);
+      if (filesz < memsz)
         pcb->max_brk = ROUNDUP(v_addr + memsz, PG_MASK);
-      }
     }
   }
-  return elf_header.e_entry;
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
@@ -94,16 +112,8 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   //这段代码有古怪，一动就会出问题，莫动
   //这个问题确实已经被修正了，TMD，真cao dan
   // 2021/12/16
-  
-  map(as, as->area.end - 8 * PAGESIZE, alloced_page - 8 * PAGESIZE, 1); 
-  map(as, as->area.end - 7 * PAGESIZE, alloced_page - 7 * PAGESIZE, 1);
-  map(as, as->area.end - 6 * PAGESIZE, alloced_page - 6 * PAGESIZE, 1); 
-  map(as, as->area.end - 5 * PAGESIZE, alloced_page - 5 * PAGESIZE, 1);
-  map(as, as->area.end - 4 * PAGESIZE, alloced_page - 4 * PAGESIZE, 1); 
-  map(as, as->area.end - 3 * PAGESIZE, alloced_page - 3 * PAGESIZE, 1);
-  map(as, as->area.end - 2 * PAGESIZE, alloced_page - 2 * PAGESIZE, 1); 
-  map(as, as->area.end - 1 * PAGESIZE, alloced_page - 1 * PAGESIZE, 1); 
-  
+  for (int i = 8 ; i >= 1 ; i--)
+      map(as, as->area.end - i * PG_SIZE, alloced_page - i * PG_SIZE, 1); 
   char *brk = (char *)(alloced_page - 4);
   intptr_t *ptr_brk = (intptr_t *)(brk);
   //这条操作会把参数的内存空间扬了，要放在最后
